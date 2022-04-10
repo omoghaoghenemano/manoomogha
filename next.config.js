8a0994232862d7f18d203c3f2d32ec1adb32bfcc
Oghenemano <oghenemano@MacBook-Pro.local>
2022-04-10 11:45:18 -0700

    NIGH

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    loader: 'default',
+    loader: 'akamai',
     path: '',
   },
   basePath:'/manoomogha',
---
+++ out/_next/static/chunks/pages/Intro-1c3d879b4fb2c33f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[289],{4603:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/Intro",function(){return u(526)}])}},function(n){n.O(0,[940,651,526,774,888,179],(function(){return _=4603,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
