3e390c6f6eb37a7ee6203e2b1e838f7946abc470
Oghenemano <oghenemano@MacBook-Pro.local>
2022-04-10 10:46:10 -0700

    adding static inage

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    loader: 'akamai',
-    path: '/manoomogha',
+    loader: 'imgix',
+    path: 'https://sandbox-uploads.imgix.net/u/1649612586-03e5fcc890055396a2f577dd5a36c457',
   },
   basePath:'/manoomogha',
   assetPrefix:'/manoomogha'
