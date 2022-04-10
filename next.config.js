7f58d0796e82a6881216390ba9b513befecfc050
Oghenemano <oghenemano@MacBook-Pro.local>
2022-04-10 10:34:20 -0700

    sike

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images:{
-    loader:"akamai",
-    path:"",
+  images: {
+    loader: 'akamai',
+    path: '/manoomogha',
   },
   basePath:'/manoomogha',
   assetPrefix:'/manoomogha'
